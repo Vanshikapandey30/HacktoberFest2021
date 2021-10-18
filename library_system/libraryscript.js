84cb80004e8053702fcb2d7f1e8e3101c7dfa282
Harman Batheja <harmanbatheja15@gmail.com>
2021-10-18 13:10:44 +0530

    Update libraryscript.js

--- library_system/libraryscript.js
+++ library_system/libraryscript.js
@@ -1,3 +1,13 @@
+// Preloader
+(() => {
+    window.addEventListener("load", function() {
+        document.querySelector(".preloader").classList.add("opacity-0");
+        setTimeout(function() {
+            document.querySelector(".preloader").style.display = "none";
+        }, 1000);
+    });
+})();
+
 console.log('This is ITSOURCECODE Project');
 class Library {
     constructor(name, author, type, price) {
