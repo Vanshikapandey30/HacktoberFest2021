104e35d3db28dc7dbcfad99b84c37ddb54f405a5
Piu05 <priyanka050412@gmail.com>
2021-10-19 18:57:44 +0530

    Added Madlib Game

---
+++ python/Madlib.py
@@ -0,0 +1,18 @@
+print("\t\tMadlib Game\n")
+A=input("Enter adjective:")  
+B=input("Enter place:")
+C=input("Enter verb:")
+D=input("Enter thing:")
+E=input("Enter greet:")
+F=input("Enter place:")
+G=input("Enter thing:")
+H=input("Enter clothing:")
+I=input("Enter verb:")
+J=input("Enter animal:")
+story=f" Amidst a beautiful morning, here comes a man with {A} appearance. He came \
+from {B}. I was {C} across the street as I went out to buy some {D} for my house. \
+I met him and he greeted me with {E}. After this meeting, I went to {F} to deliver \
+some {G}. I was in a hurry so I forgot to iron my {H}. While {I} I noticed \
+that everyone is watching me. \nI returned home after 10 minutes to check on my {J}."
+print(story)
+
